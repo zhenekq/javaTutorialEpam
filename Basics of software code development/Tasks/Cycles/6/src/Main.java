4416cc886c06434b0f42ee0595d33b30edece559
zhenekq <zhenek02ss@gmail.com>
2020-12-06 21:33:56 +0300

    6 basic cycle

---
+++ Basics of software code development/Tasks/Cycles/6/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        for(int i=0;i<=255;i++){
+            char symbol = (char) i;
+            int index = symbol;
+            System.out.println(index+" = "+symbol);
+        }
+    }
+}
