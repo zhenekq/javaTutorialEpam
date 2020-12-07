503f684677b0004784d494d28294bf4014e2747c
zhenekq <zhenek02ss@gmail.com>
2020-12-07 20:37:26 +0300

    2 algo arr-arr

---
+++ Algorithmization/Arrays of arrays/2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int squareLength = sc.nextInt();
+        int array[][] = new int[squareLength][squareLength];
+
+        for(int i=0;i<squareLength;i++){
+            for(int j=0;j<squareLength;j++){
+                array[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<squareLength;i++){
+            for(int j=0;j<squareLength;j++){
+                if(i==j)
+                    System.out.print(array[i][j] +" ");
+            }
+        }
+
+    }
+}
